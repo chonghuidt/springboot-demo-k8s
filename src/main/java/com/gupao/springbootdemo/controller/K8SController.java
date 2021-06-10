5391d0d286c53015ae2db9f7352437561f247b2d
zichang <1269653850@qq.com>
2021-06-10 14:00:33 +0800

    a

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -14,6 +14,6 @@ public class K8SController {
      */
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>111222222 ";
+        return "hello K8s <br/>111222222333 ";
     }
 }
